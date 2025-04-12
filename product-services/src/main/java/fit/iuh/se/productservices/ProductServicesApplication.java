03176c2ef73fd8177adc2e6e9946e3bdaaa847cc
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-04-13 02:40:38 +0700

    first-commit

---
+++ product-services/src/main/java/fit/iuh/se/productservices/ProductServicesApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.se.productservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServicesApplication.class, args);
+    }
+
+}
---
+++ product-services/src/test/java/fit/iuh/se/productservices/ProductServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.iuh.se.productservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
